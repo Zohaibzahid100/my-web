c99e89e9705de5459aea4ddc963b8d9d2a79acf6
zohaibzahid100 <zohibzahid7593@gmail.com>
2024-09-07 16:24:04 +0500

    sacend commit

---
+++ paractices section j query.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+    for (let j = 1 ; j <=2 ; j++){
+        $('.khkhkh'+j).click(function(){
+           $('.hghg'+j).slideToggle();
+        })
+       }
+      $('.would').click(function(){
+        $('.ppp').show()
+    })
+    $('.hide').click(function(){
+        $('.ppp').hide()
+    })
+
+    })
+   
